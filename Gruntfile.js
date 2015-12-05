900bb5877fd1d70632299d7f60e2c71a8d447d90
Rodrigo Chamun <rochamun@gmail.com>
2015-12-05 12:16:02 -0200

    Remove Jasmine & CoffeScript

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,12 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
+      options: {
+        footer: "main();"
+      },
       dist: {
         src: [ 'src/init.js', 'src/*.js' ],
         dest: 'main.js'
-      }
-    },
-    coffee: {
-      options: {
-        bare: true
-      },
-      glob_to_multiple: {
-        expand: true,
-        flatten: true,
-        src: ['spec/*.coffee'],
-        dest: 'spec/out',
-        ext: '.js'
       }
     },
     watch: {
@@ -26,21 +17,11 @@ module.exports = function(grunt) {
         tasks: ['concat', 'jade']
       }
     },
-    jade: require('./config/jade.js'),
-    jasmine: {
-      src: 'src/**/*.js',
-      options: {
-        specs: "spec/out/**/*.js"
-      }
-    }
+    jade: require('./config/jade.js')
   });
 
   grunt.registerTask('default', ['concat', 'jade']);
-  grunt.registerTask('test', ['coffee', 'jasmine']);
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jade');
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
-
 };
