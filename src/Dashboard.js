b1b8e6481bf34e225d20477560313a3babee460b
Rodrigo Chamun <rochamun@gmail.com>
2015-12-26 12:26:44 -0200

    Add points display to Dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,11 +2,16 @@ var Dashboard = (function () {
   var LIVES_SIZE = 20;
   var LIVES_GAP = 10;
   var LIVES_Y = 5;
+  var SCORE_LENGTH = 6;
 
   function Dashboard(scene) {
     this.scene = scene;
     this.points = 0;
     this.lives = 0;
+
+    this.pointsDisplay = new Text();
+    this.scene.add(this.pointsDisplay);
+    this.addPoints(0);
   };
 
   Dashboard.prototype.setLives = function(lives) {
@@ -17,6 +22,12 @@ var Dashboard = (function () {
   Dashboard.prototype.decrementLife = function() {
     if (this.lives.length == 0) throw("No more lives to decrement!");
     var life = this.lives.pop().expire();
+  };
+
+  Dashboard.prototype.addPoints = function(points) {
+    this.points += points;
+    this.pointsDisplay.setString(zeroFill(this.points.toString()));
+    this.pointsDisplay.setRight(Quick.getWidth());
   };
 
   Dashboard.prototype.getLives = function() { return this.lives.length; };
@@ -38,6 +49,14 @@ var Dashboard = (function () {
     var last = lives[lives.length - 1];
     var offset = last ? last.getRight() : 0;
     return offset + LIVES_GAP;
+  }
+
+  function zeroFill(number) {
+    var ret = [];
+    var zCount = SCORE_LENGTH - number.length;
+    while (zCount-- > 0) { ret.push(0) };
+    ret.push(number);
+    return ret.join("");
   }
 
   return Dashboard;
