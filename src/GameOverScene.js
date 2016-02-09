93375675f43513e0cb786bf39364fc365345bcf2
Rodrigo Chamun <rochamun@gmail.com>
2016-02-09 17:02:36 -0200

    GameOverScene expires after 30 seconds

--- src/GameOverScene.js
+++ src/GameOverScene.js
@@ -5,11 +5,19 @@ var GameOverScene = (function () {
     this.add(new Background(), 0);
     asteroids.forEach(this.add, this);
 
+    this.setExpiration(secondsToTicks(30));
+
     var gameOver = new GameObject();
     gameOver.setImageId("game-over");
     gameOver.setCenter(Quick.getCenter());
     this.add(gameOver, 2);
   }; GameOverScene.prototype = Object.create(Scene.prototype);
+
+  GameOverScene.prototype.getNext = function() {
+    return new GameScene();
+  };
+
+  function secondsToTicks(seconds) { return seconds * 30; }
 
   return GameOverScene;
 })();
